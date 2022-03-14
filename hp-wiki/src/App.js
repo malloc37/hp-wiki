7778ddaa0fdeec89aa57e161cb09c4f3411d0473
berliben <berliben@students.zhaw.ch>
2022-03-14 10:42:06 +0100

    Added Profile

--- hp-wiki/src/App.js
+++ hp-wiki/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import CharList from "./CharList"
+import Profile from "./Profile"
 
 function App() {
   return (
     <div className="App">
+      <Profile />
       <CharList />
     </div>
   );
